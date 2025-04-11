fae8980d38eeaaa891fa613de8234f88b05c63fd
Michael Jordan <mgj23@ic.ac.uk>
2025-04-11 10:35:22 +0100

    Adding more types of regression trees

--- RegressionTrees.R
+++ RegressionTrees.R
@@ -10,6 +10,8 @@ rm(list=ls())
 # Set working directory and load libraries
 setwd("~")
 library(tree)
+library(randomForest)
+library(gbm)
 
 # Import auto mpg data
 data <- read.table(
@@ -64,14 +66,96 @@ text(rt)
 rt
 summary(rt)
 
-# Residual mean deviance is 5.7. I.e., total residual deviance (sum of squared
+# Residual mean deviance is 9.1. I.e., total residual deviance (sum of squared
 # errors) / number of observation minus number of leaves. Seems kind of high
 # to me given that mean MPG is ~ 23.
 
 # Let's check how the model performs outside the training data
 cor.test(predict(rt, data.f[-training,]), data.f$mpg[-training])
 
-# Hey, 90% is pretty good!
+# Hey, 88% correlation is pretty good!
 
-# Check cross-validation of model
+# Evaluate residual deviance at different numbers of tree nodes
 plot(cv.tree(rt))
+
+# Looks like we could prune this tree without a huge increase in deviance.
+
+# This actually all looks pretty good, but another technique to consider is
+# bagged regression trees. The model above doesn't seem overfit, but bagged
+# regression trees deal with overfitting by dividing training data into random
+# subsets, fitting regression trees to these replicates of training data, and
+# averaging across models. The basic idea here is that by averaging across
+# models you reduce the variance among models and so reduce overfitting. I think
+# the intuitive explanation here is that there is a certain probability of bad
+# overfitting that comes with working with one subset of the data and averaging
+# across various subsets reduces the extremes of that distribution.
+
+# Build a bagged regression tree
+bart <- randomForest::randomForest(
+  mpg ~ .,
+  data = data.f[training, ],
+  mtry = ncol(data.f) -1) # Determines how many variables to consider in each
+                        # split. Here we include all variables, so making this a
+                        # bagged regression tree instead of a random forest.
+
+# Let's see if our correlation went up
+cor.test(predict(bart, data.f[-training,]), data.f$mpg[-training])
+
+# It did! 92%. Not a gigantic difference in this case, but notable.
+
+# Let's also try out a random forest model where we vary how many variables are
+# used in each replicate. This reduces the correlation between the bootstrapped
+# replicates, which should reduce overfitting even more and give us a higher
+# correlation in the final model. The basic idea is that the less similar each
+# replicate is to the other replicates, the more variance among replicates you
+# reduce when you average.
+rf <- randomForest::randomForest(
+  mpg ~ .,
+  data = data.f[training, ],
+  importance = TRUE)
+
+# Note that importance is the average decrease in the mean squared error each
+# time a split is fit to a given variable. Sort of the explanatory power of
+# each variable. Looks like modelyear is most important.
+randomForest::importance(rf)
+
+# Anyway, did we increase correlation?
+cor.test(predict(rf, data.f[-training,]), data.f$mpg[-training])
+
+# Interesting, it actually went down extremely slightly (< 1%) vs the bagged
+# tree.
+
+# Okay, last type of regression tree we'll try out, boosted regression trees.
+# They're similar to bagged and random forest models in that they fit multiple
+# models, but in this case each model is iteratively fit on the residuals of the
+# previous model. This is cool because it incorporates models fit to the
+# variation not accounted for by previous models, but results are a little hard
+# to interpret. 
+# We'll use a gaussian distribution because the data is continuous and normal-ish.
+# We can control the relative weight in final results of earlier vs later models
+# through the shrinkage parameter (lower values give later models more weight). 
+bort <- gbm::gbm(mpg ~ ., data = data.f[training,] , distribution = "gaussian")
+bort2 <- gbm::gbm(
+  mpg ~ .,
+  data = data.f[training,],
+  distribution = "gaussian",
+  shrinkage = .01)
+bort3 <- gbm::gbm(
+  mpg ~ .,
+  data = data.f[training,],
+  distribution = "gaussian",
+  shrinkage = 1)
+
+# Did we increase correlation?
+cor.test(predict(bort, data.f[-training,]), data.f$mpg[-training])
+cor.test(predict(bort2, data.f[-training,]), data.f$mpg[-training])
+cor.test(predict(bort3, data.f[-training,]), data.f$mpg[-training])
+
+# Not really. The default shrinkage parameter of .1 produces the best result,
+# extremely close to the bagged regression tree, but slightly under it.
+
+# Checking out the variables, horsepower or displacement is the most significant
+# under the boosted regression trees. 
+summary(bort)
+summary(bort2)
+summary(bort3)
